e33a8fee019903f33ea33852f8ea44f37f8529e2
Zakky109 <m.sora1226@gmail.com>
2023-10-06 00:06:01 +0900

    seo 2023-10-5 v4

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,14 +4,14 @@ exports.onPreRenderHTML = function onPreRenderHTML({
 }) {
   const headComponents = getHeadComponents();
   headComponents.sort((a, b) => {
-    if (a.type === b.type || (a.type !== 'style' && b.type !== 'style')) {
+    if (a.type === b.type || (a.type !== 'meta' && b.type !== 'meta')) {
       return 0;
     }
 
-    if (a.type === 'style') {
-      return 1;
-    } else if (b.type === 'style') {
+    if (a.type === 'meta') {
       return -1;
+    } else if (b.type === 'meta') {
+      return 1;
     }
 
     return 0;
