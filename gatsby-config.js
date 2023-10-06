e1b41af0cabf57a04bd0bd6161111cfa2d8afa1b
Zakky109 <m.sora1226@gmail.com>
2023-10-06 22:36:32 +0900

    seo 2023-10-6 v4

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     user:{ name:`Sora`, email:`bakara@yahoo.co.jp`},
     description: `技術ブログだけでなく、生活や仕事の質を向上するアイデアを公開するブログです。`,
-    image: '/defaultOgImage.png',
+    image: '/static/defaultOgImage.png',
     siteUrl: `https://emuism.netlify.app`,
     twitterUsername: `えむ@ひよっこエンジニア`,
     type:`website`,
